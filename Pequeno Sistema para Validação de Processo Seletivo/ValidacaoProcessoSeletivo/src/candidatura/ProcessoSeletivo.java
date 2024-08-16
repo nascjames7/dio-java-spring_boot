e0cdb2801bf9174cc1d5ae278364b55c643e5455
James Anderson <jan@etepd.com>
2024-08-16 13:29:47 -0300

    Projeto finalizado.

--- Pequeno Sistema para Validação de Processo Seletivo/ValidacaoProcessoSeletivo/src/candidatura/ProcessoSeletivo.java
+++ Pequeno Sistema para Validação de Processo Seletivo/ValidacaoProcessoSeletivo/src/candidatura/ProcessoSeletivo.java
@@ -9,14 +9,18 @@
 package candidatura;
 
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 public class ProcessoSeletivo {
+
     //Definição do salário base que não varia.
     static final double SALARIO_BASE = 2000.0;
     public static void main(String[] args) {
         //Chamada do  método selecaoCandidatos().
         selecaoCandidatos();
+
     }
+
     //Método estático (pois se encontra na classe principal) que seleciona os candidatos.
     static void selecaoCandidatos() {
         //Defição das variáveis.
@@ -30,6 +34,7 @@ public class ProcessoSeletivo {
         int candidatoAtual = 0;
         //Definição da variável que indica a posição do candidato a ser alocado no  Array de candidatos aprovados.
         int indiceCandidatoAprovado = 0;
+
         //Implementação do laço para contagem dos candidatos selecionados para a entrevista.
         //O laço para quando o número de candidatos selecionados atinge o valor 5 ou quando finaliza a lista de candidatos no Array.
         while((candidatosSelecionados < 5) && (candidatoAtual < candidatos.length)) {
@@ -39,6 +44,7 @@ public class ProcessoSeletivo {
             double salarioPretendido = valorPretendido();
             //Impressão para simples acompanhamento.
             System.out.println("O candidato " + candidato + " solicitou o salário de "+ salarioPretendido + ".");
+
             //Implementação do fluxo condicional para incremento do número de candidatos selecionados.
             if(salarioPretendido < SALARIO_BASE) {
                 System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
@@ -51,6 +57,7 @@ public class ProcessoSeletivo {
                 //Preenchimento do Array de candidatos aprovados.
                 candidatosAprovados[indiceCandidatoAprovado] = "REPROVADO";
             }
+
             //Incremento da posição no Array de candidatos.
             candidatoAtual++;
             //Incremento do índice de candidatosAprovados.
@@ -59,26 +66,48 @@ public class ProcessoSeletivo {
         //Impressão da lista de candidatos selecionados.
         //Definição de Array para utilização do método equals.
         String candidatosReprovados[] = new String [10];
+        int contadorAprovados = 0;
+        //Definição de Array para os candidatos aprovados que receberão as ligações do RH.
+        String apenasAprovados[] = new String [candidatosSelecionados];
+
         //Implementação do laço para preenchimento do vetor de String candidatosReprovados.
         for (int indice = 0; indice < candidatosReprovados.length; indice++) {
             candidatosReprovados[indice] = "REPROVADO";
         }
         System.out.println("\nImprimindo a lista de candidatos selecionados com o índice:\n");
+
         //Implementação do laço para impressão da lista de candidatos selecionados.
         for (int indice = 0; indice < candidatosAprovados.length; indice++) {
+
             //Implementação do fluxo condicional para impressão dos candidatos aprovados.
             if((candidatosReprovados[indice]).equals(candidatosAprovados[indice])) {
                 //Impressão apenas dos candidatos aprovados.
-            }else System.out.println("O candidato de número " + (indice + 1) + " é o " + candidatosAprovados[indice]);
+            }else {
+                System.out.println("O candidato de número " + (indice + 1) + " é o " + candidatosAprovados[indice]);
+                //Implementação do vetor que possui apenas os candidatos aprovados.
+                apenasAprovados[contadorAprovados] = candidatosAprovados[indice];
+                //Incremento do contador.
+                contadorAprovados++;
+            }
+        }
+
+        //Implementação do laço para verificar se o candidato selecionado atendeu as tentativas de contato.
+        for(String candidato: apenasAprovados) {
+            //Chamada do método entrandoEmContato(candidato);
+            entrandoEmContato(candidato);
         }
     }
+
     //Método estático que calcula o valor do salário pretendido por determinado candidato.
     static double valorPretendido () {
+
         //Retorna um valor real calculado de forma aleatória usando [ThreadLocalRandom.current().nextDouble].
         return ThreadLocalRandom.current().nextDouble(1800,2200);
     }
+
     //Método estático que analisa os candidatos em relação ao salário pretendido.
     static void analisarCandidato (double salarioPretendido) {
+
         //Implementação de fluxo condicional para análise dos candidatos.
         //Caso pretenção seja menor que o salário base.
         if(SALARIO_BASE > salarioPretendido) {
@@ -88,5 +117,41 @@ public class ProcessoSeletivo {
             System.out.println("Ligar para o candidato com uma contra proposta.");
         //Caso pretenção seja maior que o salário base.
         }else System.out.println("Aguardando o resultado dos demais candidatos.");
+    }
+
+    //Método auxiliar que verifica se o candidato atendeu o telefone em alguma das 3 tentavas de contato.
+    static boolean atender() {
+
+        //Retorna true se atendeu e false caso contrário.
+        return new Random().nextInt(3)==1;
+    }
+    //Método estático que retorna o número de tentativas ao se entrar em contato com um candidato.
+    static void entrandoEmContato(String candidato) {
+
+        //Declaração da variável que contabiliza o número de tentativas.
+        int tentativasRealizadas = 1;
+        //Declaração de variáveis de controle booleanas.
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        //Implementação do laço.
+        do {
+            //Chamada do método atender.
+            atendeu = atender();
+            //A variável abaixo assume o valor oposto da variável acima, para que o laço finalize.
+            continuarTentando = !atendeu;
+
+            //Implementação do fluxo condicional para contabilizar as tentativas realizadas.
+            if(continuarTentando) {
+                //Incremento do contador.
+                tentativasRealizadas++;
+            }else System.out.println("Contato realizado com sucesso!");
+        //Condição de parada.
+        }while(continuarTentando && tentativasRealizadas < 3);
+
+        //Implementação do fluxo condicional para imprimir o número de tentativas ao entrar em contato com os aprovados na seleção.
+        if(atendeu) {
+            System.out.println("Conseguimos contato com o candidato " + candidato + " na " + tentativasRealizadas + "º tentativa.");
+        }else System.out.println("Não conseguimos contato com o candidato " + candidato + "; número máximo de " + tentativasRealizadas + " tentativas.");
     }
 }
