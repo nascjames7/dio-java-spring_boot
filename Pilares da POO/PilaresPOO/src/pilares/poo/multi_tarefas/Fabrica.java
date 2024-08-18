cea0a1803fdd2d07f820c22be3a690b30edc394a
James Anderson <jan@etepd.com>
2024-08-17 22:51:51 -0300

    Acabou o projeto de pilares poo.

--- Pilares da POO/PilaresPOO/src/pilares/poo/multi_tarefas/Fabrica.java
+++ Pilares da POO/PilaresPOO/src/pilares/poo/multi_tarefas/Fabrica.java
@@ -2,6 +2,7 @@ package pilares.poo.multi_tarefas;
 
 import pilares.poo.multi_tarefas.equipamentos.copiadora.Copiadora;
 import pilares.poo.multi_tarefas.equipamentos.digitalizadora.Digitalizadora;
+import pilares.poo.multi_tarefas.equipamentos.digitalizadora.Scaner;
 import pilares.poo.multi_tarefas.equipamentos.impressora.DeskJet;
 import pilares.poo.multi_tarefas.equipamentos.impressora.Impressora;
 import pilares.poo.multi_tarefas.equipamentos.multifuncional.EquipamentoMultifuncional;
@@ -13,6 +14,7 @@ public class Fabrica {
         //Instanciamento.
         Impressora impressora = new DeskJet();
         EquipamentoMultifuncional em = new EquipamentoMultifuncional();
+        Scaner scaner = new Scaner();
 
 
         //Polimorfismo
