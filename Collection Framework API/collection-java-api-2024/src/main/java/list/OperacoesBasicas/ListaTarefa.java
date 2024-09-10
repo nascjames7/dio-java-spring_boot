b1fb121af01bb88cc27999f07fe4286e6f8067a8
James Anderson <jan@etepd.com>
2024-09-10 07:48:27 -0300

    Adição de classes e estrutura de coleções tipo list

---
+++ Collection Framework API/collection-java-api-2024/src/main/java/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,52 @@
+package main.java.list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> tarefaList;
+
+    /**Construtor instancia um ArrayList vazio cada vez
+     * que eu instanciar uma lista de tarefas.
+     */
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+    
+    //Método que adiciona uma tarefa.
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    //Método que remove uma tarefa.
+    public void removerTarefa(String descricao) {
+
+        //Instanciação de uma lista de tarefas.
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+
+        //Implementação do laço para detectar a tarefa a ser removida.
+        for(Tarefa tarefa : tarefaList) {
+
+            //Implementação da estrtura condicional para verficação da tarefa procurada.
+            if(tarefa.getDescricao().equalsIgnoreCase(descricao)) {
+
+                //Adicão da tarefa a ser removida para a lista tarefasParaRemover.
+                tarefasParaRemover.add(tarefa);
+            }
+        }
+        //Remoção da tarefa da lista tarefaList.
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    //Método que retorna o número total de tarefas.
+    public int obterNumeroTotalTarefas() {
+        //Retorna o tamanho da lista.
+        return tarefaList.size();
+    }
+
+    //Método que imprime a lista de tarefas a qualquer momento.
+    public void obterDescricaoTarefas() {
+        //Imprime a lista.
+        System.out.println(tarefaList);
+    }
+}
