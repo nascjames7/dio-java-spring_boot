780797a7ebc7d3e0914a5745907cb2aeb472eac2
James Anderson <jan@etepd.com>
2024-09-11 12:21:17 -0300

    Adição dos métodos de pesquisa e do método principal.

--- Collection Framework API/collection-java-api-2024/src/main/java/list/Pesquisa/CatalogoLivros.java
+++ Collection Framework API/collection-java-api-2024/src/main/java/list/Pesquisa/CatalogoLivros.java
@@ -18,7 +18,7 @@ public class CatalogoLivros {
     }
 
     //Encontra livros de um determinado autor.
-    public List<Livro> pesquisarPorInte(String autor) {
+    public List<Livro> pesquisarPorAutor(String autor) {
         /**Implementação da estrutura condicional para verificar
          * se a lista está vazia.
          */
@@ -39,6 +39,7 @@ public class CatalogoLivros {
         return livrosPorAutor;
     }
 
+    //Encontra livros dentro de um determinado intrevalo de anos.
     public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal) {
         //Instanciação de uma lista.
         List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
@@ -57,4 +58,45 @@ public class CatalogoLivros {
         return livrosPorIntervaloAnos;
     }
 
+    //Encontra livros com um determinado titulo.
+    public Livro pesquisarPorTitulo(String titulo) {
+        /**Implementação da estrutura condicional para verificar
+         * se a lista está vazia.
+         */
+        //Instanciação de um livro.
+        Livro livroPorTitulo = null;
+        //Verifica se a lista está vazia.
+        if(!livroList.isEmpty()) {
+            //Implementação do laço para localizar livro.
+            for(Livro livro : livroList) {
+                //Implementação da estrutura condicional de comparação.
+                if(livro.getTitulo().equalsIgnoreCase(titulo)) {
+                    //Adiciona o livro caso seja encontrado.
+                    livroPorTitulo = livro;
+                    //Saída do laço.
+                    break;
+                }
+            }
+        }
+        //Retorna o livro com o título especificado.
+        return livroPorTitulo;
+    }
+    //Método principal que verifica a execução dos métodos implementados.
+    public static void main(String[] args) {
+
+        //Intanciação de lista de () livros
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+
+        //Adicionando livros.
+        catalogoLivros.adicionarLivro("Biblia de Estudos", "King James", 1850);
+        catalogoLivros.adicionarLivro("Coração de Tinta", "Harrison Ford", 1999);
+        catalogoLivros.adicionarLivro("Meu Deus e Eu", "Oprah Winsley", 2010);
+        catalogoLivros.adicionarLivro("Coração de Tinta", "Harrison Ford", 1999);
+
+        //Impressao de listas de pesquisa.
+        System.out.println(catalogoLivros.pesquisarPorAutor("Oprah Winsley"));
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(1900, 2012));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Coração de Tinta"));    }
 }
+
+
